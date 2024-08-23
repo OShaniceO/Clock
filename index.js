1b44858dc0074fd9c1593235d3f0024723da6f77
OShaniceO <shanicejones744@gmail.com>
2024-08-23 14:57:31 +0100

    Updated js and fixed date and time

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
-document.getElementById("citySelect").addEventListener("change", function () {
-  const citySelect = document.getElementById("citySelect");
-  const selectedCity = citySelect.options[citySelect.selectedIndex].text;
-  const cityName = document.getElementById("cityName");
-  const cityTime = document.getElementById("cityTime");
-  const cityDate = document.getElementById("cityDate");
+function updateTime() {
+  let cityTimeZone =
+    document.querySelector("#citySelect").value || "Africa/Johannesburg";
+  let cityTime = moment().tz(cityTimeZone);
 
-  if (selectedCity !== "--Select a City--") {
-    cityName.textContent = `${selectedCity}:`;
+  let cityNameElement = document.querySelector("#cityName");
+  let cityDateElement = document.querySelector("#cityDate");
+  let cityTimeElement = document.querySelector("#cityTime");
 
-    let timeString = "12:00 PM UTC+0";
-    let dateString = "August 21, 2024";
-    switch (citySelect.value) {
-      case "Europe/Paris":
-        timeString = "12:00 PM UTC+1";
-        dateString = "August 21, 2024";
-        break;
-      case "Asia/Tokyo":
-        timeString = "08:00 PM UTC+9";
-        dateString = "August 21, 2024";
-        break;
-      case "Australia/Sydney":
-        timeString = "09:00 PM UTC+10";
-        dateString = "August 21, 2024";
-        break;
-    }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  cityNameElement.innerHTML = `${cityName}:`;
 
-    cityTime.textContent = timeString;
-    cityDate.textContent = dateString;
-  }
-});
+  cityDateElement.innerHTML = cityTime.format("MMMM Do YYYY");
+  cityTimeElement.innerHTML = cityTime.format("h:mm:ss A [UTC]Z");
+}
+
+function updateCity(event) {
+  updateTime();
+}
+
+document.querySelector("#citySelect").addEventListener("change", updateCity);
+
+updateTime();
+setInterval(updateTime, 1000);
